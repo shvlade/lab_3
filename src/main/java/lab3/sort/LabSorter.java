982071679b8ba4722036481a34fffbc43957bb65
pa1ex <a.proshaev@gmail.com>
2024-10-19 01:03:05 +0300

    init

---
+++ src/main/java/lab3/sort/LabSorter.java
@@ -0,0 +1,15 @@
+package lab3.sort;
+
+public class LabSorter {
+
+    /**
+     * Метод для сортировки массива целых чисел.
+     *
+     * @param source исходный массив для сортировки.
+     * @return отсортированный массив
+     */
+    public int[] sort(int[] source) {
+
+        throw new RuntimeException("implement me");
+    }
+}
---
+++ src/main/java/lab3/stack/LabStack.java
@@ -0,0 +1,30 @@
+package lab3.stack;
+
+/**
+ * Класс, представляющий структуру данных "Стек"
+ */
+public class LabStack {
+
+    /**
+     * Следующий элемент стека
+     */
+    private StackElement next;
+
+    /**
+     * Метод для помещения элемента в стек
+     *
+     * @param elem элемент, который необходимо поместить в стек
+     */
+    public void push(String elem) {
+        throw new RuntimeException("implement me");
+    }
+
+    /**
+     * Метод для вытаскивания элемента из стека. Если стек пуст - возвращает null
+     *
+     * @return следующий элемент стека
+     */
+    public String pop() {
+        throw new RuntimeException("implement me");
+    }
+}
